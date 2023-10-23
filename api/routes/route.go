d767beea2665fccb1dcd0f02ef8928907c60475d
Ryan Chandra Kusuma <rookie3ryan@gmail.com>
2023-10-23 17:21:38 +0700

    refactor : change package from database/sql to pgx

--- api/routes/route.go
+++ api/routes/route.go
@@ -1,13 +1,13 @@
 package routes
 
 import (
-	"database/sql"
 	"ryanchandrakusuma/wewe-base-go/config"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5"
 )
 
-func Setup(env *config.Env, timeout time.Duration, db *sql.DB, gin *gin.Engine) {
+func Setup(env *config.Env, timeout time.Duration, db *pgx.Conn, gin *gin.Engine) {
 
 }
--- config/db.go
+++ config/db.go
@@ -1,24 +1,26 @@
 package config
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
-	"log"
+	"os"
 
+	"github.com/jackc/pgx/v5"
 	_ "github.com/lib/pq"
 )
 
-func NewPqDatabase(env *Env) *sql.DB {
-	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", 
+func NewPqDatabase(env *Env) *pgx.Conn {
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
 	env.DBHost, 
 	env.DBUser, 
 	env.DBPass, 
 	env.DBName, 
 	env.DBPort)
 	
-	db, err := sql.Open("postgres", connStr)
+	db, err := pgx.Connect(context.Background(), connStr)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
 	}
 
 	return db
